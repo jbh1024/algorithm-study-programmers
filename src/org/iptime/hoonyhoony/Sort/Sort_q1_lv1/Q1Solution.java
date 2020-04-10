932e35da33837e896a68f59000e13d943ba54426
JeongByeongHun <jbh1024@gmail.com>
2020-04-10 13:06:38 +0900

    주석제거

--- src/org/iptime/hoonyhoony/Sort/Sort_q1_lv1/Q1Solution.java
+++ src/org/iptime/hoonyhoony/Sort/Sort_q1_lv1/Q1Solution.java
@@ -4,7 +4,6 @@ public class Q1Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
 
-        //System.out.println(commands.length);
         for (int i = 0; i < commands.length; i++) {
             int[] list = new int[(commands[i][1] - commands[i][0]) + 1];
             for (int j = commands[i][0] - 1; j < commands[i][1]; j++) {
