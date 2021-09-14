b907d426a1a4059f68c5424f6341aef05676212c
Jeong ByeongHun <jbh1024@gmail.com>
2021-09-14 21:39:03 +0900

    WeeklyChallenges week 1 solved.
    한줄로도 라이브러리 이용하여 더 편하게 작업할수 있음..
    ex) 등비수열..

--- src/org/iptime/hoonyhoony/WeeklyChallenges/Week1_q1/Q1Solution.java
+++ src/org/iptime/hoonyhoony/WeeklyChallenges/Week1_q1/Q1Solution.java
@@ -2,8 +2,12 @@ package org.iptime.hoonyhoony.WeeklyChallenges.Week1_q1;
 
 public class Q1Solution {
     public long solution(int price, int money, int count) {
-        long answer = -1;
+        long answer = money;
 
-        return answer;
+        for (int i = 1; i <= count; i++) {
+            answer -= (long) price * i;
+        }
+
+        return answer < 0 ? answer * -1 : 0;
     }
 }
